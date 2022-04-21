90f873c3559b64ab193d91e50a71a7f43ad4b5c7
rpremisleaner1 <rpremisleaner1@babson.edu>
2022-04-21 12:18:20 -0400

    Update working_code.py

--- working_code.py
+++ working_code.py
@@ -16,8 +16,8 @@ client = tweepy.Client(bearer_token=bearer_token)
 #     if len(tweet.context_annotations) > 0:
 #         print(tweet.context_annotations)
 
-# new_query = 'from:elonmusk boring -is:retweet -is:reply'
-new_query = 'from:elonmusk OR from:pmarca -is:retweet -is:reply'
+# new_query = 'from:elonmusk OR from:pmarca boring -is:retweet -is:reply'
+new_query = 'from:elonmusk -is:retweet -is:reply'
 start_time = '2022-04-19T00:00:00z'
 # end_time = '2022-04-21T00:00:00z'
 
@@ -28,6 +28,18 @@ new_tweets = client.search_recent_tweets(query=new_query, start_time=start_time,
 #              user_fields = ["name", "username", "location", "verified", "description"], max_results = 10, expansions='author_id')
 
 # print(new_tweets) # save as CSV or whatever to not keep querying
-print(new_tweets.data)
+# print(new_tweets.data)
+
+def create_tweet_list(tweets):
+    lst = []
+    for tweet in tweets.data:
+        lst.append(tweet.text)
+    return lst
+
+a_list = create_tweet_list(new_tweets)
+print(a_list)
+
+
+
 
 
