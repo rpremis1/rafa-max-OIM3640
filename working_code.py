95d6e795bc15bab60ae317e44840f2fea818ec55
rpremisleaner1 <rpremisleaner1@babson.edu>
2022-04-21 00:04:35 -0400

    push_working_code.py

---
+++ working_code.py
@@ -0,0 +1,28 @@
+from config import consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
+
+import tweepy
+import json
+import pandas as pd
+# import plotnine
+
+client = tweepy.Client(bearer_token=bearer_token)
+
+# query = 'from:suhemparack -is:retweet'
+
+# tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
+
+# for tweet in tweets.data:
+#     print(tweet.text)
+#     if len(tweet.context_annotations) > 0:
+#         print(tweet.context_annotations)
+
+new_query = 'from:elonmusk'
+start_time = '2022-04-15T00:00:00z'
+end_time = '2022-04-19T00:00:00z'
+
+new_tweets = client.search_recent_tweets(query=new_query, start_time=start_time, end_time=end_time, tweet_fields = ["created_at", "text", "source"],
+user_fields = ["name", "username", "location", "verified", "description"], max_results = 5, expansions='author_id')
+
+print(new_tweets) # save as CSV or whatever to not keep querying
+
+
