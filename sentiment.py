38c7e6643ce1b31a85481eb229770af6c9ee7bad
rpremisleaner1 <rpremisleaner1@babson.edu>
2022-04-24 17:18:31 -0400

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,8 +1,10 @@
+from asyncio.windows_events import NULL
 from config import consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
 from aux_functions import analyze_sentiment, create_tweet_list, get_datetime_utc, get_default_start_date, make_query
 
 import tweepy
 import pandas as pd
+from plotnine import *
 
 client = tweepy.Client(bearer_token=bearer_token)
 
@@ -19,7 +21,12 @@ new_tweets = client.search_recent_tweets(query=new_query, start_time=start_time,
 def main():
     sentiment_list = analyze_sentiment(create_tweet_list(new_tweets))
     sentiment_df = pd.DataFrame(sentiment_list, columns = ['sentiment', 'value'])
-    print(sentiment_df)
+    aggregated_df = sentiment_df.groupby(['sentiment']).mean().reset_index()
+    print((ggplot(aggregated_df, aes(y = 'value', x = 'sentiment', fill = 'sentiment', color = 'sentiment')) 
+    + geom_bar(stat = 'identity') 
+    + theme(legend_position = 'bottom', legend_title = element_blank())
+    + scale_y_continuous(minor_breaks = NULL)
+    + labs(x = '', y = '', title = 'Mean Sentiment Value (Y) vs. Sentiment Kind (X) by Sentiment Kind (Colors)', caption = f'Only tweets containing {keywords} by {usernames}')))
 
 if __name__ == '__main__':
     main()
